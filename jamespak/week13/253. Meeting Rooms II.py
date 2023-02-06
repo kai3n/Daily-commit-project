ceb8687b960c0b25c7bb27f696c242d4bac37cf7
James Pak <kaienmg@gmail.com>
2023-02-06 14:03:12 -0800

    Create 253. Meeting Rooms II.py

---
+++ jamespak/week13/253. Meeting Rooms II.py
@@ -0,0 +1,20 @@
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution:
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: int
+        """
+        intervals.sort(key=lambda x: x[0])
+        heap = []
+        for interval in intervals:
+            if heap and interval[0] >= heap[0]:
+                heapq.heapreplace(heap, interval[1])
+            else:
+                heapq.heappush(heap, interval[1])
+        return len(heap)
