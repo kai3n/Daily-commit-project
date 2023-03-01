04438fe0e3360862142ecd53cf9d33f8a41c6df4
James Pak <kaienmg@gmail.com>
2023-02-28 20:08:42 -0800

    Update 2416. Sum of Prefix Scores of Strings.py

--- jamespak/week16/2416. Sum of Prefix Scores of Strings.py
+++ jamespak/week16/2416. Sum of Prefix Scores of Strings.py
@@ -13,3 +13,38 @@ class Solution:
             ans.append(curr)
             
         return ans
+    
+# Trie solution
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.cnt = 0
+        
+class Trie:
+    def __init__(self):
+        self.trie = TrieNode()
+
+    def insert(self, word):
+        node = self.trie
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+            node.cnt += 1
+            
+    def count(self, word):
+        node = self.trie
+        ans = 0
+        for ch in word:
+            ans += node.children[ch].cnt
+            node = node.children[ch]
+        return ans
+
+class Solution:
+    def sumPrefixScores(self, A: List[str]) -> List[int]:
+        trie = Trie()
+
+        for a in A:
+            trie.insert(a)
+
+        return [trie.count(a) for a in A]
