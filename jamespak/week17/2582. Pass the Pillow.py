83efb7e8c39be3862a3f55ca04de483d942a7dae
James Pak <kaienmg@gmail.com>
2023-03-06 22:29:38 -0800

    Update 2582. Pass the Pillow.py

--- jamespak/week17/2582. Pass the Pillow.py
+++ jamespak/week17/2582. Pass the Pillow.py
@@ -1,13 +1,3 @@
 class Solution:
-    def passThePillow(self, n: int, time: int) -> int:
-        i = 0
-        direction = 0
-        while time != 0:
-            if i == 0 or i == n - 1:
-                direction ^=1 
-            if direction == 1:
-                i += 1
-            else:
-                i -= 1
-            time -= 1
-        return i + 1
+    def passThePillow(self, n, time):
+        return n - abs(n - 1 - time % (n * 2 - 2))
