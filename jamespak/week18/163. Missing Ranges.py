034596dfa403bac0cbfd1f046c949be85910c710
James Pak <kaienmg@gmail.com>
2023-03-17 20:21:36 -0700

    Create 163. Missing Ranges.py

---
+++ jamespak/week18/163. Missing Ranges.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
+        def stringify(a, b):
+            if a == b:
+                return str(a)
+            else:
+                return str(a) + "->" + str(b)
+
+        nums = [lower - 1] + nums + [upper + 1] # that's the trick to avoid edge cases
+
+        ans = [] 
+        for i in range(1, len(nums)):
+            if nums[i] - nums[i-1] > 1:
+                ans.append(stringify(nums[i - 1] + 1, nums[i] - 1))
+        return ans
