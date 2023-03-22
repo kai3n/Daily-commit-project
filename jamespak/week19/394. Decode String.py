68367482203868096517f4f742cad95fefd11287
James Pak <kaienmg@gmail.com>
2023-03-22 15:27:58 -0700

    Create 394. Decode String.py

---
+++ jamespak/week19/394. Decode String.py
@@ -0,0 +1,21 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        cur_num = 0
+        cur_string = ''
+
+        for c in s:
+            if c == '[':
+                stack.append(cur_string)
+                stack.append(cur_num)
+                cur_string = ''
+                cur_num = 0
+            elif c == ']':
+                num = stack.pop()
+                prev_string = stack.pop()
+                cur_string = prev_string + num*cur_string
+            elif c.isdigit():
+                cur_num  = cur_num*10 + int(c)
+            else:
+                cur_string +=c
+        return cur_string
