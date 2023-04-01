1f1e272561e728402a054c85ab54920472c4d69c
James Pak <kaienmg@gmail.com>
2023-04-01 15:34:40 -0700

    Update 62. Unique Paths.py

--- jamespak/week20/62. Unique Paths.py
+++ jamespak/week20/62. Unique Paths.py
@@ -1,3 +1,14 @@
+# Solution 1
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1] * n for _ in range(m)]
+        for i in range(1, len(dp)):
+            for j in range(1, len(dp[0])):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[-1][-1]
+
+# Solution 2
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         return factorial(m+n-2) // factorial(m-1) // factorial(n-1)
+
