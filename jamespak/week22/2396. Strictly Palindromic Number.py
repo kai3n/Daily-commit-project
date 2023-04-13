897aae69e5d00800fa5d6d26d3cfad12cde05132
James Pak <kaienmg@gmail.com>
2023-04-13 15:18:17 -0700

    Update 2396. Strictly Palindromic Number.py

--- jamespak/week22/2396. Strictly Palindromic Number.py
+++ jamespak/week22/2396. Strictly Palindromic Number.py
@@ -1,3 +1,14 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
         return False
+
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        for b in range(2, n-1):
+            rep = []
+            k = n
+            while k > 0:
+                rep.append(k%b)
+                k //= b
+            if rep != rep[::-1]: return False
+        return True
