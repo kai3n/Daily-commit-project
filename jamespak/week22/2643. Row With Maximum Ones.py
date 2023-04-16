db566fc651a7850f2a0c65f81ba91819e746b652
James Pak <kaienmg@gmail.com>
2023-04-16 10:41:25 -0700

    Update 2643. Row With Maximum Ones.py

--- jamespak/week22/2643. Row With Maximum Ones.py
+++ jamespak/week22/2643. Row With Maximum Ones.py
@@ -7,3 +7,7 @@ class Solution:
                 max_idx = i
                 max_cnt = sum(row)
         return [max_idx, max_cnt]
+
+class Solution:
+    def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
+        return max(enumerate(map(sum, mat)), key=lambda x:[x[1],-x[0]])
