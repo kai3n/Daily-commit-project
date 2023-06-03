ee97180e1be310dff9418957ca5e7a351cb2cd6a
James Pak <kaienmg@gmail.com>
2023-06-02 23:03:10 -0700

    Update 1823. Find the Winner of the Circular Game.py

--- jamespak/week29/1823. Find the Winner of the Circular Game.py
+++ jamespak/week29/1823. Find the Winner of the Circular Game.py
@@ -6,3 +6,16 @@ class Solution:
             i = (i + k - 1) % len(nums)
             nums.pop(i)
         return nums[0]
+
+# O(n*k) solution
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        q = deque(range(1, n + 1))
+        
+        while len(q) > 1:
+            x = k
+            while x > 1:
+                q.append(q.popleft())
+                x -= 1
+            q.popleft()
+        return q[0]
