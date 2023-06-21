241edc80226ce8a72edf16323223552a63e94e15
James Pak <kaienmg@gmail.com>
2023-06-20 22:50:58 -0700

    Create 1169. Invalid Transactions.py

---
+++ jamespak/week32/1169. Invalid Transactions.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def invalidTransactions(self, transactions):
+        res = [] #list of all the invalid transactions to return 
+        names = defaultdict(lambda: defaultdict(set)) #{name: {time: city}}
+        
+        
+        for t in transactions:
+            name,time,amount,city = t.split(",")
+            names[name][time].add(city)
+            
+        #-if a certain person is at more than one city at a time then we know it's an invalid transaction
+        #-if the city we're at is not in the set it means the transaction has been made at two places within the 60 min limit
+        
+        #I want to preface the rest of this solution by saying this has worked so far but I'm unsure if there's ever a testcase
+        #in which this would not function correctly. This was a very tricky question and it definitely took me a while 
+        #to even come up with a working solution
+        
+        for t in transactions:
+            name, time, amount, city = t.split(",")
+            if int(amount) > 1000:
+                res.append(t)            
+            else:
+                for num in names[name].keys():
+                    if -60 <= int(time) - int(num) <= 60 and (len(names[name][num]) > 1 or city not in names[name][num]):
+                        res.append(t)
+                        break
+                
+        return res
