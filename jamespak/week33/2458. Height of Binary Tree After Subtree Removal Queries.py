b5d259f8a76f9d831f0fab5590da787f433fa447
James Pak <kaienmg@gmail.com>
2023-06-27 10:59:03 -0700

    Create 2458. Height of Binary Tree After Subtree Removal Queries.py

---
+++ jamespak/week33/2458. Height of Binary Tree After Subtree Removal Queries.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def treeQueries(self, root: Optional[TreeNode], queries: List[int]) -> List[int]:
+        ans = {}
+        @cache
+        def height(r):
+            return 1 + max(height(r.left), height(r.right)) if r else 0
+        def dfs(r, depth, mx):
+            if not r: return
+            ans[r.val] = mx
+            dfs(r.left, depth + 1, max(mx, depth + height(r.right)))
+            dfs(r.right, depth + 1, max(mx, depth + height(r.left)))
+        dfs(root, 0, 0)
+        return [ans[v] for v in queries]
