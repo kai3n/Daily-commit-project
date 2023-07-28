e3a4a0ed693080b270ba5f37b25f09326b8e59c1
James Pak <kaienmg@gmail.com>
2023-07-28 09:18:30 -0700

    Update 1539. Kth Missing Positive Number.py

--- jamespak/week37/1539. Kth Missing Positive Number.py
+++ jamespak/week37/1539. Kth Missing Positive Number.py
@@ -1,3 +1,4 @@
+# O(N)
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
         arr_set = set(arr)
@@ -6,3 +7,14 @@ class Solution:
                 k -= 1
             if k == 0:
                 return i
+# O(logN)
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        l , r = 0, len(arr) # 0 1
+        while l < r:
+            m = (l + r) // 2
+            if arr[m] - m - 1 < k:
+                l = m + 1
+            else:
+                r = m
+        return r + k
