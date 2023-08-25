ac5d5484fe7569214a5689eb9e91c06e113daf59
James Pak <kaienmg@gmail.com>
2023-08-25 12:24:32 -0700

    Create 1762. Buildings With an Ocean View.py

---
+++ jamespak/week41/1762. Buildings With an Ocean View.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        cur_max = 0
+        res = []
+
+        for i in range(1, len(heights) + 1):
+            if heights[-i] > cur_max:
+                cur_max = heights[-i]
+                res.append(len(heights) - i)
+        res.sort()
+        return res
