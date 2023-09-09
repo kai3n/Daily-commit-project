1abcd35b96eb6390b1581cba1625559374e031c0
HyunDel <pshyun1331@gmail.com>
2023-09-09 15:39:21 +0900

    (프로그래머스) 숫자 게임

---
+++ Sanghyun/week43/숫자 게임.py
@@ -0,0 +1,20 @@
+def solution(A, B):
+    # 투포인트 사용 
+    a_point , b_point = 0,0
+    # B 승점 저장
+    answer = 0
+    # A,B 정렬
+    A.sort()
+    B.sort()
+
+    while a_point != len(A) and b_point != len(B):
+        # B가 크면 포인트들 다 갱신 및 B 승점 갱신
+        if B[b_point] > A[a_point]:
+            answer +=1
+            a_point+=1
+            b_point+=1
+        else:
+            # A가 크면 B포인트만 갱신
+            b_point+=1
+    # B 승점 반환
+    return answer
