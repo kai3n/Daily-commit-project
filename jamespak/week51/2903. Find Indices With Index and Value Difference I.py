5b827a49359df0d415ad0c2c1b906a508c3c1323
James Pak <kaienmg@gmail.com>
2023-11-01 16:53:40 -0700

    Update 2903. Find Indices With Index and Value Difference I.py

--- jamespak/week51/2903. Find Indices With Index and Value Difference I.py
+++ jamespak/week51/2903. Find Indices With Index and Value Difference I.py
@@ -1,3 +1,4 @@
+# O(n**2)
 class Solution:
     def findIndices(self, nums: List[int], indexDifference: int, valueDifference: int) -> List[int]:
         res = [-1, -1]
@@ -6,3 +7,19 @@ class Solution:
                 if abs(i - j) >= indexDifference and abs(nums[i] - nums[j]) >= valueDifference:
                     res = [i, j]
         return res
+
+
+# O(n)
+class Solution:
+    def findIndices(self, nums: List[int], indexDifference: int, valueDifference: int) -> List[int]:
+        mini = maxi = 0
+        for i in range(indexDifference, len(nums)):
+            if nums[i - indexDifference] < nums[mini]:
+                mini = i - indexDifference
+            if nums[i - indexDifference] > nums[maxi]:
+                maxi = i - indexDifference
+            if nums[i] - nums[mini] >= valueDifference:
+                return [mini, i]
+            if nums[maxi] - nums[i] >= valueDifference:
+                return [maxi, i]
+        return [-1, -1]
